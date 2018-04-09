f6c33b4a8c725561e94a27b0f0794941b7e1e5a1
enraged <38198875+enraged-AM@users.noreply.github.com>
2018-04-09 00:10:16 -0400

    Add files via upload

---
+++ CommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Reflection;
+
+namespace TheShooterBot
+{
+    public class CommandHandler
+    {
+        private DiscordSocketClient _client;
+        private CommandService _service;
+        
+        
+        public CommandHandler(DiscordSocketClient client)
+        {
+            _client = client;
+            
+            _service = new CommandService();
+
+            _service.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            _client.MessageReceived += HandleCommandAsync;
+
+            client.UserJoined += AnnounceJoinUser;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if (msg == null) return;
+
+            var context = new SocketCommandContext(_client, msg);
+
+            int argPos = 0;
+            if(msg.HasCharPrefix('!', ref argPos)) // Prefix for using commands, e.g., !kick Username.
+            {
+                var result = await _service.ExecuteAsync(context, argPos);
+
+                if(!result.IsSuccess)
+                {
+                    await context.Channel.SendMessageAsync(result.ErrorReason);
+                }
+
+            }
+
+        }
+        public async Task AnnounceJoinUser(SocketGuildUser user)
+        {
+            var channel = _client.GetChannel("Channel ID here.  Get rid of quotes.") as SocketTextChannel;
+
+            await channel.SendMessageAsync($"{user.Mention}, Welcome to ServerName.  Hope you enjoy your stay.");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+
+
+namespace TheShooterBot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, "Token of Bot Located Here.  Find it in Discord Developer panel.  Do NOT get rid of quotes!");
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler(_client);
+            await Task.Delay(-1);
+        }
+        
+    }
+}
